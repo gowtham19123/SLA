095e3a4214fb0a3b1a4fc80ed1940f1e1387f411
gowtham19123 <gowthamgowtham19123@gmail.com>
2026-03-31 20:16:16 +0530

    Add files via upload

---
+++ Day16 JS Task/script.js
@@ -0,0 +1,68 @@
+const _x = 95;
+const y = 80;
+let sum = _x + y;
+let sub = _x - y;
+let multi = _x * y;
+let divi = _x / y;
+let percent =_x % y; 
+console.log('sum of _x + y = '+sum);
+console.log('sub of _x - y = '+sub);
+console.log('multi of _x * y = '+multi);
+console.log('Divi of _x / y = '+divi);
+console.log('percentage of _x % y = '+percent);
+
+
+let x_name = "Lena";
+const y_name ="Kumar";
+console.log('x_name is '+x_name + "  "+'y_name '+y_name);
+let name =" John";
+
+
+const x = 10;
+const y = 15;
+if(x == y){
+    console.log('X and y are Equal');
+}
+else{
+    console.log('X and Y are Not Equal');
+}
+   
+
+
+if(1 === '1')
+{
+    console.log('Valid');
+}
+else
+{
+    console.log(' Not Valid');
+}
+
+
+
+let a = 10;
+let b = 20;
+let c = 10;
+if( a > b && a > c)
+{
+    console.log(' A is greaterthan ');
+}
+else if(b > a && b > c)
+{
+    console.log(' B is greaterthan ');
+}
+else{
+    console.log(' c is greaterthan ');
+}
+
+
+let a = 20;
+let b = 20;
+if( a = b || a > b )
+{
+    console.log(' A is greaterthan B');
+}
+else{
+    console.log(' B is greaterthan A');
+}
+
