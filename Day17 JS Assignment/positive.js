3bf1a6c0b80051219d367ee5d9c8ea9f70b52907
gowtham19123 <gowthamgowtham19123@gmail.com>
2026-04-02 18:22:02 +0530

    Add files via upload

---
+++ Day17 JS Assignment/positive.js
@@ -0,0 +1,10 @@
+let x = -21;
+if( x < 0){
+    console.log("Negative Number");
+}
+else if( x > 0){
+    console.log("Positive Number");
+}
+else{
+    console.log("pls Enter only Number")
+}
---
+++ Day17 JS Assignment/switch.js
@@ -0,0 +1,27 @@
+console.log(" 1.Monday 2.Tuesday 3.wednesday 4.Thursday 5.Friday  6.Saturday 7.Sunday");
+let x = 4;
+switch(x){
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+        default:
+            console.log("Enter a Vaild Number 1 to 6 ");
+}
