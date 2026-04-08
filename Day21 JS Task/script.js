47bdeca2021f7b2cf8fb85f92239a824b066bafc
gowtham19123 <gowthamgowtham19123@gmail.com>
2026-04-09 00:01:11 +0530

    Add files via upload

---
+++ Day21 JS Task/script.js
@@ -0,0 +1,20 @@
+
+// // double number
+// let arr = [1,2,3,4,5];
+// arr.map(item => console.log(item+item));
+
+// let arr = [1,2,3,4,5];
+// let print = arr.filter(item => item%2 == 0);
+// console.log(print);
+
+// let arr = [1,2,3,4,5];
+// let print = arr.reduce((sum , a) => sum + a,0);
+// console.log(print);
+
+// let arr = [67,21,84,1,4];
+// let print = arr.find(greater => greater > 10);
+// console.log(print);
+
+let arr = [67,21,84,1,4];
+console.log(arr.includes(84));
+console.log(arr.includes(10));
