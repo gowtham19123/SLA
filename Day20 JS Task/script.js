71c955a2ad569f367665c463ccc6b9924d9cce1c
gowtham19123 <gowthamgowtham19123@gmail.com>
2026-04-08 23:06:14 +0530

    Add files via upload

---
+++ Day20 JS Task/script.js
@@ -0,0 +1,18 @@
+// let arr =[1,2,3,4];
+// arr.push(5);
+// console.log(arr);
+
+// let arr =[1,2,3,4,5];
+// arr.pop();
+// console.log(arr);
+
+
+// let arr =[1,2,3,4,5];
+// arr.unshift("name");
+// console.log(arr);
+
+let arr = ["sno","name","age",4,5]
+for(let i=0;i < arr.length;i++){
+   console.log(arr[i]);
+}
+ 
